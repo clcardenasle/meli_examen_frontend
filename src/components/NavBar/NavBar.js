67feb3c35345d811cb9e49b600a999b269c26f96
Carol <carol_cardenas93@hotmail.com>
2021-09-14 14:08:05 -0500

    add materialUI sections

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,30 @@
 import React from "react";
 import styles from './NavBar.module.scss';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import InputBase from "@material-ui/core/InputBase";
+import { Grid } from "@material-ui/core";
 
 const NavBar = () => {
-
   return (
-    <div className={styles.bg}>NavBar</div>
-  )
+    <Grid container xs={12}>
+      <AppBar>
+        <Toolbar className={styles.navBar}>
+          <Grid item xs={1}>
+            Icon
+          </Grid>
+          <Grid item xs={9}>
+            <InputBase
+              placeholder="Nunca dejes de buscar"
+              inputProps={{ "aria-label": "search" }}
+            >
+            </InputBase>
+              <div>boton</div>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Grid>
+  );
 };
 
 export default NavBar;
