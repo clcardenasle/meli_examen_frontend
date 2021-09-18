91352f029955ca8fc2d87e9895b504d77267d4e4
Carol <carol_cardenas93@hotmail.com>
2021-09-18 11:53:39 -0500

    add error handling

--- src/components/NavBar/NavBar-Constants.js
+++ src/components/NavBar/NavBar-Constants.js
@@ -4,3 +4,8 @@ export const URL = {
 };
 
 export const PLACEHOLDER = "Nunca dejes de buscar";
+
+export const Images = {
+  logo: "/assets/img/logo_ML.png",
+  icSearch: "/assets/img/ic_Search@2x.png",
+};
